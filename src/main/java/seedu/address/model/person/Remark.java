a1bf868ee4ddcb59d0b1502cfdbaf25c42c159cd
WayneTanKinLun <waynetan2000@gmail.com>
2020-09-23 20:51:48 +0800

    Edit code following gradle checkstyle

--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -9,6 +9,10 @@ import static java.util.Objects.requireNonNull;
 public class Remark {
     public final String value;
 
+    /**
+     * Remark constructor takes in a string as remark.
+     * @param remark
+     */
     public Remark(String remark) {
         requireNonNull(remark);
         value = remark;
